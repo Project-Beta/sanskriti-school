4b2bcf29a3476f502a0d3f8f2b7c16f34fb53343
KSL <lordharambae@gmail.com>
2017-11-07 13:23:57 +0530

    added js/cssbeautify

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+const gulp = require("gulp");
+const pug = require("pug");
+const stylus = require("stylus");
+const babel = require("babel-cli");
+const htmllint = require("htmllint");
+const csslint = require("csslint");
+const jslint = require("jslint");
+const cssbeautify = require("cssbeautify");
+const jsbeautify = require("jsbeautify");
+const xtag = require("x-tag");
+
+//implement
