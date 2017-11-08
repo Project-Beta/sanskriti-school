a6d77a8a627d5c6dea8efba58ad4592a904e283f
Mihir Chaturvedi <plibither8@gmail.com>
2017-11-08 10:39:55 +0530

    Modified gulpfile; improved X-Browser Compatibility

--- dist/js/navigation.js
+++ dist/js/navigation.js
@@ -1,12 +1,1 @@
-var linkContainer = document.querySelector('div.linkContainer');
-var header = document.querySelector('div.header');
-var headerHeight = header.clientHeight;
-window.addEventListener('scroll', function() {
-	var scrollHeight = window.scrollY;
-	if (scrollHeight > headerHeight) {
-		linkContainer.style.position = 'fixed';
-		linkContainer.style.top = '0';
-	} else {
-		linkContainer.style.position='static'
-	}
-})
+'use strict';var linkContainer=document.querySelector('div.linkContainer');var header=document.querySelector('div.header');var headerHeight=header.clientHeight;window.addEventListener('scroll',function(){var scrollHeight=window.scrollY;if(scrollHeight>headerHeight){linkContainer.style.position='fixed';linkContainer.style.top='0'}else{linkContainer.style.position='static'}});
--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,15 @@
-// const babel = require("gulp-babel");
 // const cssbeautify = require("gulp-cssbeautify");
 // const csslint = require("gulp-csslint");
-const gulp = require("gulp");
 // const htmllint = require("gulp-html");
 // const jsbeautify = require("gulp-esformatter");
 // const jslint = require("jslint");
+// const uglify = require("gulp-uglify");
+// const xtag = require("x-tag");
+const babel = require("gulp-babel");
+const gulp = require("gulp");
 const pug = require("gulp-pug");
 const stylus = require("gulp-stylus");
-// const xtag = require("x-tag");
-// const uglify = require("gulp-uglify");
-
-//implement
+const nib = require("nib");
 
 gulp.task('pug', function() {
 	return gulp.src('src/pug/*.pug')
@@ -21,9 +20,21 @@ gulp.task('pug', function() {
 gulp.task('stylus', function() {
 	return gulp.src('src/styl/*.styl')
 	.pipe(stylus({
+		use: [nib()],
 		compress: true
 	}))
 	.pipe(gulp.dest('dist/css'));
 })
 
-gulp.task('default', ['pug', 'stylus'])
+gulp.task('scripts', function() {
+	return gulp.src('src/js/*.js')
+	.pipe(babel({
+		presets: ['env'],
+		minified: true
+	}))
+	.pipe(gulp.dest('dist/js'))
+})
+
+gulp.task('default', ['pug', 'stylus', 'scripts'], function () {
+	gulp.watch(['src/pug/**/*.pug', 'src/styl/**/*.styl', 'src/js/**/*.js'], ['pug', 'stylus', 'scripts']);
+})
